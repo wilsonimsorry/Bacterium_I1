636c18d903ec2a80b6dac3ea5b85b195dcf8bb78
wilsonimsorry <gstwilson@hotmail.com>
2015-07-18 21:07:43 +0100

    Update bact.js

--- bact.js
+++ bact.js
@@ -375,10 +375,10 @@ function infoButton() {
 
 
 	if (infoPress === true) {
-		text("X", 54.5, 60);
+		text("X", 55.5, 60);
 		infoBox();
 	} else if (infoPress === false) {
-		text("?", 54.5, 60);
+		text("?", 55.5, 60);
 	}
 
 
